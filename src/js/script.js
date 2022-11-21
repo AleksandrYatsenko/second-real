4c8b497bc4d13d7a29ef6fe8ce121335929b26f7
Alex <yatsikxvii@gmail.com>
2022-11-21 13:50:45 +0200

    finish burger

--- src/js/script.js
+++ src/js/script.js
@@ -1,58 +1,55 @@
-//  ----------- Up -------------
-const elemUp = document.querySelector('.up');
-window.addEventListener('scroll', () => {
-  if(window.scrollY >= 1300) {
-    elemUp.classList.remove('hidden');
-  } else if (window.scrollY <= 1300) {
-    elemUp.classList.add('hidden');
+document.addEventListener('DOMContentLoaded', () => {
+
+  //  ----------- Up -------------
+  const elemUp = document.querySelector('.up');
+  window.addEventListener('scroll', () => {
+    if(window.scrollY >= 1300) {
+      elemUp.classList.remove('hidden');
+    } else if (window.scrollY <= 1300) {
+      elemUp.classList.add('hidden');
+    }
+  });
+
+  //  -------------- burger -----------------
+  const header = document.querySelector('.header'), 
+        burger = document.querySelector('.header__burger'),
+        navBlock = header.querySelector('.header__nav'),
+        navItems = navBlock.querySelectorAll('.header__link'),
+        headerButton = header.querySelector('.button'),
+        undlayForNav = document.querySelector('.underlayerForNav');
+
+  function toggleBurger() {
+    burger.classList.toggle('header__burger_active');
+    navBlock.classList.toggle('header__nav_active');
+    undlayForNav.classList.toggle('hidden');
+    document.body.classList.toggle('lock'); 
+  };
+
+  function closeBurger() {
+    navBlock.classList.remove('header__nav_active');
+    burger.classList.remove('header__burger_active');
+    undlayForNav.classList.add('hidden');
+    document.body.classList.remove('lock');
+
   }
+
+  burger.addEventListener('click', toggleBurger);
+
+  navItems.forEach(item => {
+    item.addEventListener('click', closeBurger);
+  });
+
+  document.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.classList.contains('underlayerForNav') || target == headerButton) {
+      closeBurger();
+      console.log(target);
+    }
+  });
+
 });
 
-//  -------------- burger -----------------
-const header = document.querySelector('.header'), 
-      burger = document.querySelector('.header__burger'),
-      navBlock = header.querySelector('.header__nav'),
-      navItems = navBlock.querySelectorAll('.header__link'),
-      headerButton = header.querySelector('.button'),
-      undlayForNav = document.querySelector('.underlayerForNav');
 
-// function toggleBurger () {
-//   burger.classList.toggle('header__burger_active');
-//   navBlock.classList.toggle('header__nav_active');
-// };
-function openBurger() {
-  burger.classList.add('header__burger_active');
-  navBlock.classList.add('header__nav_active');
-  undlayForNav.style.display = 'block';
-}
-function closeBurger() {
-  navBlock.classList.remove('header__nav_active');
-  burger.classList.remove('header__burger_active');
-  undlayForNav.style.display = 'none';
-}
-burger.addEventListener('click', (e) => {
-  console.log(e.target);
-  if (!burger.classList.contains('header__burger_active')) {
-    openBurger();
-  }
-});
 
-header.addEventListener('click', (e) => {
-  const target = e.target;
-  if (target.classList.contains('header__burger_active') || target.classList.contains('underlayerForNav') || target == headerButton) {
-    closeBurger();
-    console.log(target);
-  }
-});
 
-// burgerSpans.forEach(item => {
-//   item.addEventListener('click', toggleBurger);
-// });
-// burgerSpans.forEach(item => {
-//   item.addEventListener('click', closeBurger);
-// })
-
-navItems.forEach(item => {
-  item.addEventListener('click', closeBurger);
-});
 
